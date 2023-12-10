4b98896de3243e5004ca6a62a22778e65a1ae264
Satya Kalyan <satyakalyan5@gmail.com>
2023-12-09 22:26:59 -0700

    Added TS mini project

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,17 +1,46 @@
-"use strict";
 // console.log("Woohoo yeah!!!");
-// function printDouble(msg: string){
-//     console.log(msg);
-//     console.log(msg);
-// }
-// printDouble("HELLO WORLD!");
-// const btn = document.getElementById("btn");
-// console.log(btn)
-// let mystery: unknown = "Hello World!";
-// const numChars = (mystery as string).length
-var btn = document.getElementById("btn");
-var input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return (JSON.parse(todosJSON));
+}
+const todos = readTodos();
+todos.forEach(createTodo);
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    //creating local Storage
+    localStorage.setItem("items", JSON.stringify(todos));
     input.value = "";
-});
+}
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function () {
+        console.log("CLICKED!");
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
+export {};
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     console.log("SUBMITTED!");
+// });
+// btn.addEventListener("click", function() {
+//     alert(input.value);
+//     input.value = "";
+// });
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,5 +1,7 @@
 // console.log("Woohoo yeah!!!");
 
+import { create } from "domain";
+
 // function printDouble(msg: string){
 //     console.log(msg);
 //     console.log(msg);
@@ -13,10 +15,65 @@
 // let mystery: unknown = "Hello World!";
 // const numChars = (mystery as string).length
 
+//WATCH FROM 73rd LECTURE TO GET THIS WORKING
+
+interface Todo {
+    text: string,
+    completed: boolean;
+}
+
 const btn = document.getElementById("btn") as HTMLButtonElement;
 const input = document.getElementById("todoinput") as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;  
 
-btn.addEventListener("click", function() {
-    alert(input.value);
+function readTodos(): Todo[] {
+    const todosJSON = localStorage.getItem("todos");
+    if(todosJSON === null) return [];
+    return (JSON.parse(todosJSON));
+}
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    };
+
+    createTodo(newTodo);
+    todos.push(newTodo);
+
+    //creating local Storage
+    localStorage.setItem("items", JSON.stringify(todos));
+
     input.value = "";
-});
+}
+
+function createTodo(todo: Todo){
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+        console.log("CLICKED!");
+    });
+    
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     console.log("SUBMITTED!");
+// });
+
+// btn.addEventListener("click", function() {
+//     alert(input.value);
+//     input.value = "";
+// });
